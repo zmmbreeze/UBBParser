12db7598ab83d609c6695be9bfb09594ef6b6f25
mzhou <zmmbreeze0825@gmail.com>
2012-02-23 01:07:16 +0800

    增加js

--- src/ubb.js
+++ src/ubb.js
@@ -1,5 +1,85 @@
-window.UBBParser = function () {
+var UBBParser = function () {
+    var Node = function( tag, content, attrs, parent, children ) {
+        this.tag = tag;
+        this.content = content;
+        this.attrs = attrs;
+        this.parent = parent;
+        this.children = children;
+    };
+    Node.prototype.ubb = function () {
+        return '';
+    };
+    Node.prototype.html = function () {
+        return '';
+    };
+    var parse$Node = function( $node ) {
+            var nodeName = $node[0].nodeName.toLowerCase(),
+                re = {};
+            switch( nodeName ) {
+                case 'img':
+                    re.content = $node.data('src');
+                    // 处理图片
+                    if ( !re.content ) {
+                        re.content = $node.attr('src');
+                        re.tag = 'image';
+                    // 处理视频
+                    } else {
+                        re.tag = 'video';
+                    }
+                    break;
+                case 'embed':
+                    // 处理swf
+                    break;
+                case 'a':
+                    // 处理a标签
+                    re.tag = 'link';
+                    re.attrs = {
+                        href: $node.attr('hre');
+                    };
+                    re.content = $node.text();
+                    break;
+                case 'blockquote':
+                    // 处理blockquote
 
+                    break;
+                default:
+                    var isBold = $node.css('font-weight') === 'bold',
+                        isItalic = $node.css('font-weight') === 'italic';
+                    if ( isBold ) {
+                        re.tag = 'bold';
+                        re.content = $node.text();
+                    } else if ( isItalic ) {
+                        re.tag = 'italic';
+                        re.content = $node.text();
+                    } else {
+                        re.isText = true;
+                        re.content = $node.text();
+                    }
+                    break;
+            }
+            return re;
+        },
+        parseHtml = function( $node ) {
+            var $children = node.contents()
+                node = parse$Node( $node, $children ),
+                children = [],
+                l = $children.length;
+            for( var i=0; i<l; i++ ) {
+                children.push( parseHtml( $children.eq(i) ); );
+            }
+            node.children = children;
+            return node;
+        },
+        rendHtml = function( node ) {
+            
+        },
+        parseUbb = function( ubb ) {
+            
+        },
+        rendUbb = function( node ) {
+
+        };
+/*
     // extend
     function extend ( Super, Son ) {
         for (var method in Super.prototype) {
@@ -13,7 +93,7 @@ window.UBBParser = function () {
         for ( var i=0,l=array.length; i<l; i++ ) {
             tmp = array[i];
             args = ' ' + tmp.key + '=' + tmp['key'];
-        }
+       }
     }
 
     function Tag() {
@@ -68,4 +148,5 @@ window.UBBParser = function () {
             
         };
     };
+*/
 }();
